48228ae386fe6479e54a2436c90472fe2562a384
zachmanac <zachstephenson94@gmail.com>
2023-03-01 12:21:49 -0800

    add empty and header components with stories

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,10 +1,13 @@
 import React from "react";
-import "components/Appointment/styles.scss"
+import "components/Appointment/styles.scss";
+
 
 export default function Appointment(props) {
 
 
   return (
-    <article className="appointment"></article>
+    <div>
+      <article className="appointment"></article>
+    </div>
   );
 };
