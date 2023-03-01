3da19adb41bdf0ae5f9106ee439e50b7756174f3
zachmanac <zachstephenson94@gmail.com>
2023-03-01 12:59:56 -0800

    add confirm and show components with stories

--- stories/index.js
+++ stories/index.js
@@ -13,6 +13,8 @@ import InterviewerList from "components/InterviewerList";
 import Appointment from "components/Appointment";
 import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
+import Show from "components/Appointment/Show";
+import Confirm from "components/Appointment/Confirm";
 
 
 storiesOf("Button", module)
@@ -148,5 +150,20 @@ storiesOf("Appointment", module)
   .add("Empty", () => (
     <Empty 
       onAdd={action("onAdd")}
+    />)
+  )
+  .add("Show", () => (
+    <Show 
+      student="Lydia Miller-Jones"
+      interviewer={interviewer}
+      onEdit={action("onEdit")}
+      onDelete={action("onDelete")}  
+    />)
+  )
+  .add("Confirm", () => (
+    <Confirm 
+      message="Delete the appointment?"
+      onConfirm={action("onConfirm")}
+      onCancel={action("onCancel")}
     />)
   )
