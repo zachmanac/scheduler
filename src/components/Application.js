491965c3cdf3958676780bbb89ea1cdfa1686ba1
zachmanac <zachstephenson94@gmail.com>
2023-03-03 10:38:23 -0800

    remove days array, install axios and add axios.get request

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,31 +1,62 @@
-import React, { useState } from "react";
-
+import React, { useState, useEffect } from "react";
 import "components/Application.scss";
-
 import DayList from "./DayList";
+import Appointment from "./Appointment";
+import axios from "axios";
 
-const days = [
-  {
+
+const appointments = {
+  "1": {
     id: 1,
-    name: "Monday",
-    spots: 2,
+    time: "12pm",
   },
-  {
+  "2": {
     id: 2,
-    name: "Tuesday",
-    spots: 5,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer:{
+        id: 3,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
   },
-  {
+  "3": {
     id: 3,
-    name: "Wednesday",
-    spots: 0,
+    time: "2pm",
   },
-];
+  "4": {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Archie Andrews",
+      interviewer:{
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg",
+      }
+    }
+  },
+  "5": {
+    id: 5,
+    time: "4pm",
+  }
+};
 
 export default function Application(props) {
 
   const [day, setDay] = useState("Monday");
-  // const [interviewer, setInterviewer] = useState();
+  
+  
+  const [days, setDays] = useState([]);
+  
+  useEffect(() => {
+    const url = `http://localhost:8001/api/days`
+    axios.get(url).then(response => {
+      setDays([...response.data])
+    })
+}, [day])
 
   return (
     <main className="layout">
@@ -50,7 +81,12 @@ export default function Application(props) {
     />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+    
+        {Object.values(appointments).map(appointment => <Appointment 
+          key={appointment.id} 
+          {...appointment} 
+        />)}
+        <Appointment key="last" time="5pm" />
       </section>
     </main>
   );
