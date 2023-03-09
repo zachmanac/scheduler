ba011341e87ddcbdd50f5cfd806ac6c97b1755da
zachmanac <zachstephenson94@gmail.com>
2023-03-09 13:11:22 -0800

    add useApplicationData to hold state logic, move from application to useStateApplicationData

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,59 +1,18 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "./Appointment";
-import axios from "axios";
 import { getAppointmentsForDay, getInterview, getInterviewersForDay } from "../helpers/selectors";
+import useApplicationData from "hooks/useApplicationData";
 
 export default function Application(props) {
-  
-  const setDay = day => setState({ ...state, day });
 
-  const [state, setState] = useState({
-    day: "Monday",
-    days: [],
-    appointments: {},
-    interviewers: {}
-  });
-
-  function bookInterview(id, interview) {
-    console.log("bookinterview", id, interview);
-    const appointment = {
-      ...state.appointments[id],
-      interview: { ...interview }
-    };
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment
-    };
-
-    return axios.put(`/api/appointments/${id}`, {interview})
-    .then(() => {
-      setState({
-        ...state,
-        appointments: appointments
-      })
-    });
-  }
-
-  function cancelInterview(id, interview) {
-    const appointment = {
-      ...state.appointments[id],
-      interview: null
-    };
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment
-    };
-    
-    return axios.delete(`/api/appointments/${id}`, {interview})
-    .then(() => {
-      setState({
-        ...state,
-        appointments: appointments
-      })
-    });
-  }
+  const {
+    state,
+    setDay,
+    bookInterview,
+    cancelInterview
+  } = useApplicationData();
 
   const dailyAppointments = getAppointmentsForDay(state, state.day);
   const dailyInterviewers = getInterviewersForDay(state, state.day);
@@ -72,19 +31,6 @@ export default function Application(props) {
       />
     );
   });
-
-
-
-  useEffect(() => {
-    Promise.all([
-      axios.get("/api/days"),
-      axios.get("api/appointments"),
-      axios.get("api/interviewers")
-    ])
-    .then((all) => {
-      setState(prev => ({...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data}))
-    })
-  }, [])
 
   return (
     <main className="layout">
---
+++ src/hooks/useApplicationData.js
@@ -0,0 +1,67 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+export default function useApplicationData () {
+
+  const setDay = day => setState({ ...state, day });
+
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {},
+    interviewers: {}
+  });
+
+  function bookInterview(id, interview) {
+    console.log("bookinterview", id, interview);
+    const appointment = {
+      ...state.appointments[id],
+      interview: { ...interview }
+    };
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment
+    };
+
+    return axios.put(`/api/appointments/${id}`, {interview})
+    .then(() => {
+      setState({
+        ...state,
+        appointments: appointments
+      })
+    });
+  }
+
+  function cancelInterview(id, interview) {
+    const appointment = {
+      ...state.appointments[id],
+      interview: null
+    };
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment
+    };
+    
+    return axios.delete(`/api/appointments/${id}`, {interview})
+    .then(() => {
+      setState({
+        ...state,
+        appointments: appointments
+      })
+    });
+  }
+
+  useEffect(() => {
+    Promise.all([
+      axios.get("/api/days"),
+      axios.get("api/appointments"),
+      axios.get("api/interviewers")
+    ])
+    .then((all) => {
+      setState(prev => ({...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data}))
+    })
+  }, [])
+
+  return { setDay, bookInterview, cancelInterview, state }
+  
+}
