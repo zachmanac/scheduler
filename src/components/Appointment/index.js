43663b9d2b970b9c9f728672c4bb18164d68aa4d
zachmanac <zachstephenson94@gmail.com>
2023-03-24 00:13:13 -0700

    fix bug with error_save mode

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -30,7 +30,7 @@ export default function Appointment(props) {
       student: name,
       interviewer
     };
-    transition(SAVING, true);
+    transition(SAVING);
     props.bookInterview(props.id, interview)
     .then(() => {
       transition(SHOW)
